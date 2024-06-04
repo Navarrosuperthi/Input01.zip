14c15d95c47a386a9b7850dbd7bcef4d99ac02c6
FabioBritto <fabio.tritono@gmail.com>
2024-06-04 17:05:32 -0300

    Commit project Input01.zip

---
+++ src/Input01.java
@@ -0,0 +1,6 @@
+import javax.swing.JOptionPane;
+public class Input01 {
+    public static void main(String[] args) {
+        JOptionPane.showInputDialog("Digite algo:"); // ira aparecer uma menagem para digitar algo
+    }
+}
